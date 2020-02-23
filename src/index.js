1aa5f8235ff3b9e6d5472ce68ea1cffe90b39432
Quazaru <qilaur@mail.ru>
2020-02-23 16:42:03 +0300

    fix: fixed algorithm to 8 passes

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-function towelSort(matrix) {
-    let sorted = [];  
-        for(let i = 0; i < matrix.length; i++){
-            let tempo = matrix[i];
-            if( i % 2 == 0)
-            {
-                for (let j = 0; j < tempo.length; j++) {
-                    sorted.push(tempo[j]);
-                    
-                }
-            }else 
-            {
-                tempo.reverse();
-                for (let j = 0 ; j < tempo.length; j++) {
-                    
-                    sorted.push(tempo[j]);
-                    
-                }
 
-            }
-            
-            
-        }
-    return sorted;
-}
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix == undefined){return []}
+  let sorted = [];  
+  for(let i = 0; i < matrix.length; i++){
+      let tempo = matrix[i];
+      if( i % 2 == 0)
+      {
+          for (let j = 0; j < tempo.length; j++) {
+              sorted.push(tempo[j]);
+              
+          }
+      }else 
+      {
+          tempo.reverse();
+          for (let j = 0 ; j < tempo.length; j++) {
+              
+              sorted.push(tempo[j]);
+              
+          }
+
+      }
+      
+      
+  }
+matrix = sorted;
+return sorted;
 }
