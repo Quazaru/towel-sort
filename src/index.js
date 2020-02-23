3438a961406ab331e96f48565c6300ab0a2ed53a
Quazaru <qilaur@mail.ru>
2020-02-23 16:27:13 +0300

    feat: add algorithm solution

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
+function towelSort(matrix) {
+    let sorted = [];  
+        for(let i = 0; i < matrix.length; i++){
+            let tempo = matrix[i];
+            if( i % 2 == 0)
+            {
+                for (let j = 0; j < tempo.length; j++) {
+                    sorted.push(tempo[j]);
+                    
+                }
+            }else 
+            {
+                tempo.reverse();
+                for (let j = 0 ; j < tempo.length; j++) {
+                    
+                    sorted.push(tempo[j]);
+                    
+                }
 
+            }
+            
+            
+        }
+    return sorted;
+}
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
